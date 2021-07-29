899d0805894ddead49dc51757945c0124520c4e8
Naman Chandok <namanchandok1@gmail.com>
2021-07-29 10:57:14 +0530

    Update users.sql

--- users.sql
+++ users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 26, 2021 at 06:10 PM
+-- Generation Time: Jul 27, 2021 at 05:00 PM
 -- Server version: 10.4.19-MariaDB-cll-lve
 -- PHP Version: 7.2.32
 
@@ -39,10 +39,17 @@ CREATE TABLE `users` (
   `points` int(11) NOT NULL,
   `hintca` tinyint(1) NOT NULL,
   `htmlno` int(11) NOT NULL,
-  `timest` timestamp DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `timest` timestamp NULL DEFAULT current_timestamp(),
   `proceed` tinyint(1) NOT NULL,
   `themeno` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `school`, `email`, `username`, `password`, `lvl`, `points`, `hintca`, `htmlno`, `timest`, `proceed`, `themeno`) VALUES
+(1172, 'Pushan Banerjee', 'The Mother\'s International School', 'pushanban@gmail.com', 'nerdy', '$2y$10$eEMPhFwvZ67ounuwksP2x.cuwidg/wRwSdebkaRqLyDv69eQlgL8O', 0, 0, 1, 0, '2021-07-23 01:32:03', 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -64,7 +71,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1357;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
